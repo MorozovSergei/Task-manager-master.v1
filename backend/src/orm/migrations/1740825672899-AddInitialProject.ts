f23fc14fab1f1464ab2bed8c8625a1a4df02d921
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2025-03-02 13:15:22 +0500

    first commit

---
+++ backend/src/orm/migrations/1740825672899-AddInitialProject.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddInitialProject1740825672899 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ backend/src/orm/migrations/1740825689443-AddInitialIdea.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddInitialIdea1740825689443 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
